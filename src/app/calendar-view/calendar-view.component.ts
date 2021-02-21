9a393fbe031ce7760795ce42eaeabb673a2f5d50
Gabbyton <gponon2000@gmail.com>
2021-02-21 11:19:36 -0500

    added div height fixed self-previous issue

--- src/app/calendar-view/calendar-view.component.ts
+++ src/app/calendar-view/calendar-view.component.ts
@@ -7,20 +7,37 @@ import { CalendarService } from '../utils/calendar.service';
   styleUrls: ['./calendar-view.component.css']
 })
 export class CalendarViewComponent implements OnInit {
-  daysOfWeek : string[];
-  timesOfDay : string[];
-  shouldDisplayCell : any;
 
-  constructor( private calendarService : CalendarService ) { }
+  previousCell: { prevRow: number, prevCol: number };
+  daysOfWeek: string[];
+  timesOfDay: string[];
+  shouldDisplayCell: any;
+
+  constructor(private calendarService: CalendarService) { }
 
   ngOnInit(): void {
     this.daysOfWeek = this.calendarService.getDaysOfWeek();
     this.timesOfDay = this.calendarService.getTimesOfDay();
-    this.shouldDisplayCell = this.calendarService.generateCellDisplayMatrix(this.timesOfDay.length,this.daysOfWeek.length);
+    this.shouldDisplayCell = this.calendarService.generateCellDisplayMatrix(this.timesOfDay.length, this.daysOfWeek.length);
   }
 
-  onCellClick(row:number,col:number) {
+  onCellClick(row: number, col: number) {
     this.shouldDisplayCell[row][col] = true;
+    if (this.previousCell != null) {
+      if (this.previousCell != null && (row != this.previousCell.prevRow || col != this.previousCell.prevCol))
+        this.shouldDisplayCell[this.previousCell.prevRow][this.previousCell.prevCol] = false;
+    }
+    this.previousCell = { prevRow: row, prevCol: col };
+    console.log(this.previousCell);
+    // TODO : disable setting previous when on the same cell
   }
 
+  getDivHeight( timeDuration? : number ) : string {
+    if( timeDuration != null && timeDuration < 0 ) // TODO : allow extending to multiple days
+      throw new Error('time duration must be a positive number! ex. 10, 60, 100 for 10 minutes, 60 minutes, 100 minutes, etc.');
+    else if( timeDuration == null )
+      timeDuration = 60; // setddefault time duration here
+    let divHeight = `${(timeDuration/this.calendarService.getSubdivisionCount())*100}%`;
+    return divHeight;
+  }
 }
--- src/app/utils/calendar.service.ts
+++ src/app/utils/calendar.service.ts
@@ -14,8 +14,13 @@ export class CalendarService {
     'Saturday',
     'Sunday'
   ]
-
   private timesOfDay: string[];
+
+  public subdivisionCount : number;
+  public is24HourFormat : boolean;
+
+  private DEFAULT_IS24HOURFORMAT : boolean = false;
+  private DEFAULT_SUBDIVISIONCOUNT : number = 30;
 
   constructor() { }
 
@@ -23,31 +28,61 @@ export class CalendarService {
     return this.daysOfWeek;
   }
 
-  public getTimesOfDay(): string[] {
-    if (this.timesOfDay == null)
-      this.timesOfDay = this.generateTimesOfDay();
+  public getTimesOfDay( is24HourFormat? : boolean , subdivisionCount? : number ): string[] {
+    if( this.timesOfDay == null )
+      this.setTimesOfDay(is24HourFormat , subdivisionCount);
     return this.timesOfDay;
   }
 
-  private generateTimesOfDay(): string[] {
+  private setTimesOfDay(is24HourFormat? : boolean , subdivisionCount? : number) : void {
+      this.is24HourFormat = is24HourFormat;
+      this.subdivisionCount = subdivisionCount;
+      this.timesOfDay = this.generateTimesOfDay( is24HourFormat , subdivisionCount );
+  }
+
+  private generateTimesOfDay(is24HourFormat?: boolean, subdvision?: number): string[] {
+    if (subdvision != null && 60 % subdvision !== 0)
+      throw new Error('Subdivision must be a factor of 60!! ex. 5, 10, 12 for every 5 minutes, every 10 minutes, every 12 minutes, etc.');
+    else if( subdvision == null )
+      subdvision = this.DEFAULT_SUBDIVISIONCOUNT;
+
+    if( is24HourFormat == null )
+      is24HourFormat = this.DEFAULT_IS24HOURFORMAT;
+
     let timesOfDay = [];
-    for (let hour = 0; hour < 24; hour++) {
-      timesOfDay.push(`${hour}:00`);
-      timesOfDay.push(`${hour}:30`);
-    }
-    console.log(timesOfDay);
+      for (let hour = 0; hour < 24; hour++) {
+        for( let min = 0 ; min < 60 ; min += subdvision ) {
+          timesOfDay.push( `${ hour == 0 || hour == 12 ? '12' : ( hour - 12 > 0 ? hour - 12 : hour ) }:${`${min}`.padStart(2,'0')}${ is24HourFormat ? '' : `${hour - 12 >= 0 ? 'PM' : 'AM' }` }` );
+        }
+      }
     return timesOfDay;
   }
 
   public generateCellDisplayMatrix(rowCount: number, colCount: number) {
     let displayMatrix = [];
-    for( let i = 0 ; i < rowCount ; i ++ ) {
-      let rowArray : boolean[] = [];
-      for( let j = 0 ; j < colCount ; j++ ) {
+    for (let i = 0; i < rowCount; i++) {
+      let rowArray: boolean[] = [];
+      for (let j = 0; j < colCount; j++) {
         rowArray.push(false);
       }
       displayMatrix.push(rowArray);
     }
     return displayMatrix;
+  }
+
+  public getIs24HourFormat() : boolean {
+    if( this.is24HourFormat == null ) {
+      console.warn( 'The is24HourFormat fieild is null. Set to default value true...' );
+      return this.DEFAULT_IS24HOURFORMAT;
+    }
+    return this.is24HourFormat;
+  }
+
+  public getSubdivisionCount() : number {
+    if( this.subdivisionCount == null ) {
+      console.warn('The subdvisionCount field is null. Set to default value to 60 (minutes)...' );
+      return this.DEFAULT_SUBDIVISIONCOUNT;
+    }
+    return this.subdivisionCount;
   }
 }
